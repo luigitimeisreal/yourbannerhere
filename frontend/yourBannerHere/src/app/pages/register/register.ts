709bdcdaaa283352a31455560d2b15c838d12dbc
Luigitime! <antoniors2003@gmail.com>
2025-07-14 20:55:42 +0200

    Added alert component

--- frontend/yourBannerHere/src/app/pages/register/register.ts
+++ frontend/yourBannerHere/src/app/pages/register/register.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { Alert } from '../../components/alert/alert';
 
 @Component({
   selector: 'app-register',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ReactiveFormsModule, Alert],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
 export class Register {
 
+  notCoincidentPassword = false;
+  errorsInFields = false;
+
+  errors = [
+    {type: "required", message: "Field must not be empty"},
+    {type: "maxlength", message: "Field length is too big"},
+    {type: "minlength", message: "Field length is too small"},
+    {type: "email", message: "Field must be an email"},
+  ]
+
+  userData = new FormGroup({
+    username: new FormControl("", [Validators.required, Validators.maxLength(16), Validators.minLength(2)]),
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required]),
+    passwordConfirmation: new FormControl("", [Validators.required])
+  })
+
+  register() {
+    console.log(this.userData.value);
+    if (this.userData.value.password !== this.userData.value.passwordConfirmation) {
+      this.notCoincidentPassword = true;
+      setTimeout(() => {
+        this.notCoincidentPassword = false;
+      }, 4000);
+    }
+    if (!this.userData.valid) {
+      this.errorsInFields = true;
+      setTimeout(() => {
+        this.errorsInFields = false;
+      }, 4000);
+    }
+  }
 }
